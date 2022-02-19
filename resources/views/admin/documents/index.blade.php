ad95deabd5830a3470ccc52230d040085b50efa8
hanq <hanq@deha-soft.com>
2022-02-19 17:29:45 +0700

    fix layout

--- resources/views/admin/documents/index.blade.php
+++ resources/views/admin/documents/index.blade.php
@@ -22,7 +22,6 @@
                     <div class="small-box bg-info">
                         <div class="inner">
                             <h3>150</h3>
-
                             <p>Hồ sơ</p>
                         </div>
                         <div class="icon">
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -11,7 +11,7 @@
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <li class="nav-item">
+                <li class="nav-item menu-open">
                     <a href="#" class="nav-link">
                         <i class="nav-icon far fa-envelope"></i>
                         <p>
